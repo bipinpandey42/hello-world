c943ad037febfbe6fe1b005438f783c75879312e
Source Academy User <bipinpandey42>
2023-01-04 15:50:50 +0530

    Changes made from Source Academy

--- sicp_01.js
+++ sicp_01.js
@@ -36,4 +36,25 @@ function sqrt(x) {
     return sqrt_iter(1, x);
 }
 
-sqrt(100 + 37);
+function cube(x) {
+    return x * x * x;
+}
+
+function improve_cube(guess, x) {
+    return ((x / square(guess)) + (2 * guess)) / 3;
+}
+
+function is_good_enough_cube(guess, x) {
+    
+    return relative_error(guess, improve_cube(guess, x)) < 0.001;
+}
+
+function cbrt_iter(guess, x) {
+    return is_good_enough_cube(guess, x)
+           ? guess
+           : cbrt_iter(improve_cube(guess, x), x);
+}
+
+function cube_root(x) {
+    return cbrt_iter(1, x);
+}
